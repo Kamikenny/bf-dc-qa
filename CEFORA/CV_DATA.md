f7b093e9fbaaf8191daa8acd534142b0d06932c7
Kenny Sirichantho <76822808+Kamikenny@users.noreply.github.com>
2026-08-18 15:58:30 +0200

    added failed letters display

--- CEFORA/CV_DATA.md
+++ CEFORA/CV_DATA.md
@@ -3,21 +3,27 @@
 
 ### Experiences :
 **2008 - 2025 :** Hospitality worker through different positions
-- **Waiter :** 
-
-- **Bartender :**
-
+- **Bartender / Waiter :**
+    - Customer oriented : Communication, conflict management, flexibility
+    - Tasks oriented : Analysis, stress management
 - **Manager :**
-
+    - Team oriented : Communication, support, adaptation
+    - Tasks oriented : Problem solving, organization
+    
 ### Certifications & Courses :
-- **2007**
-- **2007**
-- **2007**
+- **2007 :** Graduated form High School in Social Sciences
+- **2013 - 2016 :** Various Hospitality related courses and certifications
 
 ### Key Strengths : 
-- **Curious :**
-- **Curious :**
-- **Curious :**
-- **Curious :**
+- **Curious**
+- **Team spirit**
+- **Stress management**
+- **Flexibility**
+- **Communication**
 
 ### Languages :
+- French : C2 / Native
+- English : C1
+
+### Skills :
+- **Web :** HTML / CSS / React.js - Basics
--- Web/HTML_CSS_JS/v3/scripts/example.js
+++ Web/HTML_CSS_JS/v3/scripts/example.js
@@ -1,3 +1,114 @@
-console.log("Hello World")
-console.log("big résumé")
+console.log("Jeu du pendu !");
 
+//! Récuperation du formulaire dans le DOM de la page
+const gameForm = document.getElementById('game-form');
+const msgGameForm = document.getElementById('message-game-form');
+const displayWord = document.getElementById('le-mot-à-trouver');
+const failedLetters = document.getElementById('failed-letters');
+
+//! Variable de stockage
+const letterAlreadySubmit = new Set();
+let mysteryWord;
+let lettersFound;
+
+//! Setup du jeu
+function startGame() {
+    // TODO Rendre aleatoire le choix de mot
+    mysteryWord = ['S', 'O', 'L', 'E', 'I', 'L'];
+    lettersFound = [];
+    // Reset des lettres envoyées
+    letterAlreadySubmit.clear();
+    updateDisplayWord();
+}
+startGame();
+
+//! Réaction à la validation du formulaire
+gameForm.addEventListener('submit', function (event) {
+    // Annulation du comportement par defaut => Refresh
+    event.preventDefault();
+
+    // Récuperer la valeur (depuis le form)
+    // - La balise "input" via son "name"
+    const userInput = gameForm['user-input'];
+    // - On lit la valeur contenu
+    const letter = userInput.value.toUpperCase();
+    console.log(letter);
+
+    // Traitement de la lettre
+    if(letter.length !== 1) {
+        msgGameForm.textContent = 'La lettre invalide';
+    }
+    else if(letterAlreadySubmit.has(letter)) {
+        msgGameForm.textContent = `La lettre ${letter} a déjà été proposé ! Boulet ♥`;
+    }
+    else {
+        letterAlreadySubmit.add(letter);
+        
+        if(checkLetterIsValid(letter)) {
+            msgGameForm.textContent = `La lettre ${letter} est dans le mot`;
+            updateDisplayWord();
+        }
+        else {
+            msgGameForm.textContent = `La lettre ${letter} n'est pas dans le mot`;
+            displayFailedLetters();
+        }
+    }
+    
+    // Efface la valeur de l'input
+    userInput.value = '';
+    
+    // On continue ?
+    if(checkGameOver()) {
+        msgGameForm.textContent = `Bravo, vous avez gagné`;
+    }
+});
+
+function checkLetterIsValid(letter) {
+    if(mysteryWord.includes(letter)) {
+        lettersFound.push(letter);
+        return true;
+    }
+    return false;
+}
+
+function updateDisplayWord() {
+    displayWord.innerHTML = '';
+
+    for(const letter of mysteryWord) {
+
+        // Création d'un balise "span" en JS (Pas afficher)
+        const span = document.createElement('span');
+
+        // Moficiation du contenu du "span"
+        if(lettersFound.includes(letter)) {
+            span.textContent = letter;
+        }
+        else {
+            span.textContent = '_';
+        }
+
+        // Ajoute la balise "span" à la balise "p"
+        displayWord.append(span);
+    }
+}
+
+function displayFailedLetters() {
+    displayFailedLetters.innerHTML = '';
+    for (const letter of letterAlreadySubmit) {
+
+        // Création d'un balise "span" en JS (Pas afficher)
+        const span = document.createElement('span');
+            span.textContent = letter;
+
+        // Ajoute la balise "span" à la balise "p"
+        failedLetters.append(span);
+    }
+}
+
+function checkGameOver() {
+    // Les letters du mots (sans doublon)
+    const mysteryWordSet = new Set(mysteryWord);
+    const lettersFoundSet = new Set(lettersFound);
+
+    return lettersFoundSet.isSupersetOf(mysteryWordSet);
+}
